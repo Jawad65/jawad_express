bb4b372221458b0e9ec47b13e60227eae597f663
jawad65 <jawadabuajabal@gmail.com>
2021-10-04 01:53:40 +0300

    This is a new commit for what I originally planned to be amended

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+
+//Student Name: Jawad Abu-Gabal
+//Student Number: 301184700
+//Date: OCT 1st, 2021
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
